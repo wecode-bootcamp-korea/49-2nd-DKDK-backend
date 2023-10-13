06b16b9a57b9a91aaa4a3542bdf6a879e1456ab3
Seungwon Kim <kim.sw94@gmail.com>
2023-10-13 12:53:53 +0900

    fix: 소숫점형태의 운동기록 데이터를 분/시간단위로 변환하는 함수를 생성하였습니다.

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -2,12 +2,14 @@ const recordService = require('../services/recordService');
 
 const readRecord = async(req, res) => {
     const requestReadRecord = req.params;
+    const currentDate = new Date();
     const { id, createdDate } = requestReadRecord;
 }
 
 
 const createRecord = async(req, res) => {
     const requestCreateRecord = req.body;
+    const currentDate = new Date();
     const { id, createdDate } = requestCreateRecord;
 }
 
--- src/services/recordService.js
+++ src/services/recordService.js
@@ -8,6 +8,15 @@ const createRecordService = async(requestCreateRecord) => {
 
 const readRecordService = async(requestReadRecord) => {
 
+
+
+    const convertToHoursAndMinutes(decimal) => {
+        const hours = Math.floor(decimal);
+        const minutes = (decimal - hours) * 60;
+        return `${hours}시간 ${Math.round(minutes)}분`;
+    }
+    
+    const result = convertToHoursAndMinutes(id);
 }
 
 module.exports = {
