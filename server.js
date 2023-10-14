834c2d561c98e0d67326b00221c9014437b1eeca
JunwooPark <wnsdn1994@gmail.com>
2023-10-15 00:36:14 +0900

    feat: test환경 구축, 모듈추가

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ require("dotenv").config();
 const { createApp } = require("./app");
 const { AppDataSource } = require("./src/models/dataSource");
 
-//server test
-app.get("/ping", async (req, res) => {
-  return res.status(200).json({ message: "pong" });
-});
-
 // 서버를 시작하는 함수
 const start = async () => {
   const app = createApp();
@@ -16,6 +11,11 @@ const start = async () => {
   await AppDataSource.initialize().then(() =>
     console.log("Datasource initialized.")
   );
+
+  //server test
+  app.get("/ping", async (req, res) => {
+    return res.status(200).json({ message: "pong" });
+  });
 
   app.listen(port || 8000, () => {
     console.log(`server is running at ${app.get("port")}`);
