bcf934d7db10de82644a35f38082ac95a693aa40
Seungwon Kim <kim.sw94@gmail.com>
2023-10-16 21:41:17 +0900

    fix: 분기처리가 되었습니다.

--- src/services/recordService.js
+++ src/services/recordService.js
@@ -1,6 +1,6 @@
 const recordDao = require("../models/recordDao");
 const { throwError } = require("../utils");
-const {  
+const {
   testDao,
   recordCreator,
   musclemassReader,
@@ -12,112 +12,101 @@ const {
   weightReader,
   recordUpdater,
   recordChecker,
-  bodyfatReader } = recordDao;
+  bodyfatReader,
+} = recordDao;
 
 const createRecordService = async (addRecord) => {
+  try {
+    const nowDate = new Date();
+    const userDateTime = await recordDao.recordChecker(addRecord);
+    const receivedDateTime = userDateTime[0].createdDate; 
+    const dateCheckerPole = nowDate.setHours(0, 0, 0, 0); 
+    const formattedUserDate = receivedDateTime.setHours(0,0,0,0)
   
-  const getCurrentDate = () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, '0');  // Month is 0-indexed, so +1 is needed
-    const day = String(today.getDate()).padStart(2, '0');
-    const dateNow = `${year}-${month}-${day}`;
-    return dateNow
-  }
+    // if (dateCheckerPole !== formattedUserDate) {
+    //   const recordCreator = await recordDao.recordCreator(addRecord);
+    //   return recordCreator;
+    // }
 
-  const getFormattedDate = () => {
-    const userday = Date();
-    const year = String(userday.getFullYear());
-    const month = String(userday.getMonth() + 1).padStart(2, '0');  // Month is 0-indexed, so +1 is needed
-    const day = String(userday.getDate()).padStart(2, '0');
-    const dateUser = `${year}-${month}-${day}`;
-    return dateUser
-  }
-
+    if (dateCheckerPole !== formattedUserDate) {
+      const recordCreator = await recordDao.recordCreator(addRecord);
+      return recordCreator;
+    }
+      const recordUpdater = await recordDao.recordUpdater(addRecord);
+      return recordUpdater;
+    
+  };
+} catch (error) {
+  console.log(error);
+}
   
-  const currentDate = getCurrentDate();
-  console.log(currentDate)
-  const userDate = await recordDao.recordChecker(addRecord);
-  const receivedDate = userDate[0].createdDate;
-  console.log(receivedDate)
-  const userDateChecker = getFormattedDate(receivedDate)
-  console.log(userDateChecker)
-  
-
-  if (userDateChecker !== currentDate) {
-    const recordCreator = await recordDao.recordCreator(addRecord);
-    return recordCreator;
-  } 
-    const recordUpdater = await recordDao.recordUpdater(addRecord);
-    return recordUpdater;
-};
 
 const readRecordService = async (id) => {
   const fatRecordReader = await recordDao.bodyfatReader(Number(id));
-  const numberFatRecords = fatRecordReader.map(record => {
-    const numberFat = Number(record.bodyFat)
+  const numberFatRecords = fatRecordReader.map((record) => {
+    const numberFat = Number(record.bodyFat);
     return {
       bodyFat: numberFat,
-      createdDate: record.createdDate
+      createdDate: record.createdDate,
     };
   });
-  
+
   const muscleRecordReader = await recordDao.musclemassReader(Number(id));
-  const numberMuscleRecords = muscleRecordReader.map(record => {
-    const numberMuscleMass = Number(record.muscleMass)
+  const numberMuscleRecords = muscleRecordReader.map((record) => {
+    const numberMuscleMass = Number(record.muscleMass);
     return {
       muscleMass: numberMuscleMass,
-      createdDate: record.createdDate
+      createdDate: record.createdDate,
     };
   });
-  
+
   const workoutTimeReader = await recordDao.timeReader(Number(id));
-  const numberWorkoutTimeRecords = workoutTimeReader.map(record => {
-    const numberWorkout = Number(record.workoutTime)
+  const numberWorkoutTimeRecords = workoutTimeReader.map((record) => {
+    const numberWorkout = Number(record.workoutTime);
     return {
       workoutTime: numberWorkout,
-      createdDate: record.createdDate
+      createdDate: record.createdDate,
     };
   });
-  
+
   const heartbeatReader = await recordDao.maxHeartbeatReader(Number(id));
-  const numberHeartbeatRecords = heartbeatReader.map(record => {
-    const numberHeartbeat = Number(record.maxHeartrate)
+  const numberHeartbeatRecords = heartbeatReader.map((record) => {
+    const numberHeartbeat = Number(record.maxHeartrate);
     return {
       maxHeartbeat: numberHeartbeat,
-      createdDate: record.createdDate
+      createdDate: record.createdDate,
     };
   });
 
   const bmiRecordReader = await recordDao.bmiReader(Number(id));
-  const numberBmiRecords = bmiRecordReader.map(record => {
+  const numberBmiRecords = bmiRecordReader.map((record) => {
     const weightSquared = parseFloat(record.weight) * parseFloat(record.weight);
     const bmi = weightSquared / parseFloat(record.height);
     return {
       bmi: Number(bmi.toFixed(2)), // 결과를 소수점 둘째 자리까지만 반환
-      createdDate: record.createdDate
+      createdDate: record.createdDate,
     };
   });
 
   const avgTimeTotal = await recordDao.avgWorkoutTimeTotal();
-    const avgTimeUser = await recordDao.avgWorkoutTimeUser(Number(id));
-    const formattedAvgTimeTotal = Number(avgTimeTotal[0].workoutTime);
-    const formattedAvgTimeUser = Number(avgTimeUser[0].workoutTime);
-    const numberCompareTime = {
+  const avgTimeUser = await recordDao.avgWorkoutTimeUser(Number(id));
+  const formattedAvgTimeTotal = Number(avgTimeTotal[0].workoutTime);
+  const formattedAvgTimeUser = Number(avgTimeUser[0].workoutTime);
+  const numberCompareTime = {
     avgTimeTotal: formattedAvgTimeTotal,
-    avgTimeUser: formattedAvgTimeUser
-  }
+    avgTimeUser: formattedAvgTimeUser,
+  };
 
   const weightRecordReader = await recordDao.weightReader(Number(id));
 
   const rawRecordReader = {
     numberMuscleRecords,
     numberBmiRecords,
-    numberFatRecords ,
+    numberFatRecords,
     numberHeartbeatRecords,
     numberWorkoutTimeRecords,
     numberCompareTime,
-  }  
+  };
 
   return rawRecordReader;
 };
@@ -125,7 +114,7 @@ const readRecordService = async (id) => {
 const testService = async (id) => {
   console.log("HELLO FROM THE SERVICE");
   const testReader = await recordDao.testDao(Number(id));
-  return testReader
+  return testReader;
 };
 
 module.exports = {
