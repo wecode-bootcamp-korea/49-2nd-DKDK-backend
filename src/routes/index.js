ff07da8fff32167330fcda674d376e3c8fba7a70
kroonstazy <70473564+kroonstazy@users.noreply.github.com>
2023-10-19 12:45:06 +0900

    RESTful - Entity convention "Update src/routes/index.js"
    Co-authored-by: Soheon Lee <42701133+soheon-lee@users.noreply.github.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
 
-const { mypageRouter } = require("./mypageRouter");
+const userHealthInfoRouter = require("./userHealthInfoRouter");
 
 const routes = express.Router();
 // try-catch문에 대해서 err catch 해주는 부분이 라우터 단에서 필요하다.
 
-routes.use("/mypage", mypageRouter);
+routes.use("/userHealthInfo", userHealthInfoRouter);
 
 module.exports = { routes };
