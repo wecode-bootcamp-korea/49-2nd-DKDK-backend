96c6366c8e5cb131d4dd43db49ebd8eddcf3d9ee
Rooney Lee <kroonstazy@gmail.com>
2023-10-19 13:19:22 +0900

    style: router명을 행정명이 아닌 Entity 기준으로 변경

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const userHealthInfoRouter = require("./userHealthInfoRouter");
+const { userHealthInfoRouter } = require("./userHealthInfoRouter");
 
 const routes = express.Router();
 // try-catch문에 대해서 err catch 해주는 부분이 라우터 단에서 필요하다.
