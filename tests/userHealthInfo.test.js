a48537b160cbce63dab7bf79c180bfecc8b0f882
Rooney Lee <kroonstazy@gmail.com>
2023-10-19 14:48:49 +0900

    test: test.js - beforeAll 에 테스트에 필요한 유저 정보 생성

--- tests/userHealthInfo.test.js
+++ tests/userHealthInfo.test.js
@@ -12,13 +12,22 @@ describe("GET User Information", () => {
     // 모든 테스트가 시작하기 전(beforeAll)에 app을 만들고, DataSource를 이니셜라이징 합니다.
     app = createApp();
     await AppDataSource.initialize();
+    
+    // GET 테스트를 위한 유저 정보 생성
+    await AppDataSource.query(
+      `INSERT INTO users 
+      (nickname, birthday, gender, phone_number, user_type, height, weight, interested_workout, workout_load)
+        VALUES
+      ('테스터', '2023-10-10', 1, 01012341234, 1, 186.00, 78.00, 1, 1);
+      `);
+
   });
 
   afterAll(async () => {
     // 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다.
     // 테이블에 있는 데이터를 날려주는 코드
-    // await AppDataSource.query(`TRUNCATE users`);
-
+    await AppDataSource.query(`DELETE FROM users WHERE nickname = '테스터';`);
+    
     // 모든 테스트가 끝나게 되면(afterAll) DB 커넥션을 끊어줍니다.
     await AppDataSource.destroy();
   });
@@ -34,7 +43,7 @@ describe("GET User Information", () => {
 
   test("FAILED: GET - NO SUCH USER", async () => {
     const res = await request(app)
-      .get("/userHealthInfo?userId=13")
+      .get("/userHealthInfo?userId=999999")
       .expect(400)
 
       expect(res.body.message).toEqual("KEY_ERROR_NO_SUCH_USER");
@@ -43,14 +52,21 @@ describe("GET User Information", () => {
   // 다음과 같이 본인이 작성한 코드에 맞춰 다양한 케이스를 모두 테스트해야 합니다.
   // 그래야 의도에 맞게 코드가 잘 작성되었는지 테스트 단계에서부터 확인할 수 있습니다!
   test("SUCCESS: GET - INFO_LOADED", async () => {
+    // TEST DB에는 개발/서비스 DB에 있는 정보가 없을 수 있기 때문에 SELECT문을 활용,
+    // beforeAll에서 생성한 user정보를 불러와 user.id를 추출 -> GET요청에 함수형으로 넣음
+    // 이유: user.id는 auto_increment이기 때문에, 생성/삭제가 반복되면 pk인 id는 계에속 올라감
+    const userInfo = await AppDataSource.query(
+      `
+      SELECT id FROM users WHERE nickname = '테스터'
+      `);
+    
     const res = await request(app)
-      .get("/userHealthInfo?userId=4&grade=1600")
+      .get(`/userHealthInfo?userId=${userInfo[0].id}`)
     //   .expect(200)
     expect(200)
     expect(res.body.message).toEqual('MYPAGE_LOADED')
     expect(res.body).toHaveProperty('data')
     //   .expect({ message: 'MYPAGE_LOADED'});
   });
-
 
 });
