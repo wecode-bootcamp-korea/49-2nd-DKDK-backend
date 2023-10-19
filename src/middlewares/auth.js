8afdc9f208612a9c5304751053c207ebb7b14e1e
JunwooPark <wnsdn1994@gmail.com>
2023-10-19 16:57:40 +0900

    fix: comment 내용 반영

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,6 +1,5 @@
 const jwt = require("jsonwebtoken");
 const { throwError } = require("../utilities/throwError");
-//.env 파일에 SECRET 항목을 추가해줘야 합니다.
 const { SECRET } = process.env;
 
 const validateToken = async (req, res, next) => {
