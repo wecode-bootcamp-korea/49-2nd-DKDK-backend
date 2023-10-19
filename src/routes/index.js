ae75a318bc7ce5c8cc69489c652a2380d04505fc
JunwooPark <wnsdn1994@gmail.com>
2023-10-20 01:53:22 +0900

    fix : middleware, utils index제거

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ const express = require("express");
 const { userHealthInfoRouter } = require("./userHealthInfoRouter");
 
 const routes = express.Router();
-// try-catch문에 대해서 err catch 해주는 부분이 라우터 단에서 필요하다.
 
 routes.use("/userHealthInfo", userHealthInfoRouter);
 
