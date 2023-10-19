11bce8f852e76b52321403c93141577da2b3c8e2
JunwooPark <wnsdn1994@gmail.com>
2023-10-19 14:33:08 +0900

    feat : trainerM Service 예외 처리, 기능 구현

--- src/services/trainerMatchingService.js
+++ src/services/trainerMatchingService.js
@@ -1,11 +1,14 @@
 const { trainerMatchingDao } = require("../models");
+const { throwError } = require("../utils/throwError");
 
 const getTrainerProduct = async (userId) => {
   //트레이너 확인 후 토큰 확인해서 구독 여부
   const isTrainer = trainerMatchingDao.isTrainer(userId);
   const isSubscript = trainerMatchingDao.isSubscript(userId);
-  if (isTrainer && isSubscript) {
+  if (isTrainer || isSubscript) {
+    throwError(400, "IS_NOT_SUBSCIBE");
   }
+  return await trainerMatchingDao.getTrainerMatching();
 };
 
 module.exports = { getTrainerProduct };
