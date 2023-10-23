6f70ce1d5203c6b8de60575e887e53f2f310836e
hyeonwoo Jeong <heywoo12@gmail.com>
2023-10-23 19:10:49 +0900

    feat: 트레이너 결제 추가

--- src/services/subscriptionService.js
+++ src/services/subscriptionService.js
@@ -1,24 +1,38 @@
 const { throwError } = require("../utils/throwError");
-const { subscripntionDao } = require("../models")
+const { subscripntionDao } = require("../models");
 const { checkSubscriptionAndCreateSubOrder } = subscripntionDao;
+const { userDao } = require("../models");
+const { findByUserId } = userDao;
 
 const checksubscriptionValidity = async (userId, name, amount) => {
-
-    //데이터베이스에 저장되어 있는 구독권 이름인지
+      // 데이터베이스에 저장되어 있는 구독권 이름인지
     if (name.includes("DKDK 커뮤니티 1개월 구독권")) {
-        name = 1;
+      name = 1;
     } else {
-        throwError(400, "INVALID_NAME")
+      throwError(400, "INVALID_NAME");
+     }
+
+      // userType에 해당하는 금액이 맞는지
+      const result = await findByUserId(userId)
+      const findUserType = result.user_type
+      console.log("findUserType : ", findUserType)
+
+      const userTypes = {
+        USER: "1",
+        TRAINER: "2",
+      };
+
+      if (findUserType === userTypes.USER && amount !== 10000) {
+        throwError(400, "INVALID_AMOUNT");
+    } else if (findUserType === userTypes.TRAINER && amount !== 20000) {
+        throwError(400, "INVALID_AMOUNT");
     }
 
-    // userType = 1 이면 만원, 2면 2만원 결제하는 거 맞는지 확인
-    
-       //유효한 구독권인지 확인
-       const issubscriptionValid = await checkSubscriptionAndCreateSubOrder(userId, name, amount);
-       return issubscriptionValid;
-}
+      //유효한 구독권인지 확인
+     const issubscriptionValid = await checkSubscriptionAndCreateSubOrder(userId, name, amount);
+     return issubscriptionValid;
+    };
 
-module.exports = {
-    checksubscriptionValidity,
-
-}
+    module.exports = {
+     checksubscriptionValidity,
+    };
