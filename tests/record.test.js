67dcea2790e3523900a39e29141b694af82a6972
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2023-10-23 23:42:12 +0900

    fix: 경로를 수정하였습니다. 테스트 코드의 틀을 잡았습니다.

--- tests/record.test.js
+++ tests/record.test.js
@@ -1,248 +1,99 @@
-// tests/user.test.js
+// // tests/user.test.js
 
-// npm i --save-dev supertest
-const request = require("supertest");
+// // npm i --save-dev supertest
+// const request = require("supertest");
+// const nock = require("nock");
 
-// supertest의 request에 app을 담아 활용하기 위해 createApp 함수를 불러옵니다.
-const { createApp } = require("../app");
-// DB와의 커넥션을 위해 DataSource 객체를 불러옵니다.
-const { AppDataSource } = require("../src/models/dataSource");
+// // supertest의 request에 app을 담아 활용하기 위해 createApp 함수를 불러옵니다.
+// const { createApp } = require("../app");
+// // DB와의 커넥션을 위해 DataSource 객체를 불러옵니다.
+// const { AppDataSource } = require("../src/models/dataSource");
 
-describe("read Records", () => {
-  let app;
+// describe("read Records", () => {
+//   let app;
+//   const mockedId = "1";
+//   const mockedImgUrl = "https://example.com/profile_image.jpg";
+//   const mockedAccessToken = "mocked_access_token";
 
-  beforeAll(async () => {
-    // 모든 테스트가 시작하기 전(beforeAll)에 app을 만들고, DataSource를 이니셜라이징 합니다.
-    app = createApp();
-    await AppDataSource.initialize().then(() => {
-      console.log("test db initialized");   
-    });
-  });
+//   beforeAll(async () => {
+//     // 모든 테스트가 시작하기 전(beforeAll)에 app을 만들고, DataSource를 이니셜라이징 합니다.
+//     app = createApp();
+//     await AppDataSource.initialize().then();
 
-  afterAll(async () => {
-    // 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다.
-    // await AppDataSource.query(`TRUNCATE workout_records`);
-    // 모든 테스트가 끝나게 되면(afterAll) DB 커넥션을 끊어줍니다.
-    await AppDataSource.destroy();
-  });
+//     nock("https://kauth.kakao.com")
+//     .post("/oauth/token")
+//     .reply(200, {
+//       access_token: mockedAccessToken,
+//     });
 
-  test("FAILED: Unable to find the user", async () => {
-    await request(app)
-    .get("/records/readRecord/6")
-    .expect(400)
-    .expect('"NO_USER"')
-  });
+//     nock("https://kapi.kakao.com")
+//       .get("/v2/user/me")
+//       .query({
+//         access_token: mockedAccessToken,
+//       })
+//       .reply(200, {
+//         id: mockedId,
+//         properties: {
+//           profile_image: mockedImgUrl,
+//         },
+//       });
 
-  test("SUCCESS: Able to read the user record", async () => {
-    await request(app)
-    .get("/records/readRecord/3")
-    .expect(200)
-    .expect({
-      "numberMuscleRecords": [
-          {
-              "muscleMass": 23,
-              "createdAt": "2023-10-17"
-          },
-          {
-              "muscleMass": 41,
-              "createdAt": "2023-10-07"
-          },
-          {
-              "muscleMass": 41,
-              "createdAt": "2023-10-06"
-          },
-          {
-              "muscleMass": 40,
-              "createdAt": "2023-10-05"
-          },
-          {
-              "muscleMass": 39,
-              "createdAt": "2023-10-04"
-          },
-          {
-              "muscleMass": 38,
-              "createdAt": "2023-10-03"
-          },
-          {
-              "muscleMass": 35,
-              "createdAt": "2023-10-02"
-          },
-          {
-              "muscleMass": 34,
-              "createdAt": "2023-10-01"
-          },
-          {
-              "muscleMass": 33,
-              "createdAt": "2023-09-30"
-          }
-      ],
-      "numberBmiRecords": [
-          {
-              "bmi": 33.56,
-              "createdAt": "2023-10-17"
-          },
-          {
-              "bmi": 27.8,
-              "createdAt": "2023-10-07"
-          },
-          {
-              "bmi": 27.8,
-              "createdAt": "2023-10-06"
-          },
-          {
-              "bmi": 29.39,
-              "createdAt": "2023-10-05"
-          },
-          {
-              "bmi": 31.86,
-              "createdAt": "2023-10-04"
-          },
-          {
-              "bmi": 34.42,
-              "createdAt": "2023-10-03"
-          },
-          {
-              "bmi": 35.3,
-              "createdAt": "2023-10-02"
-          },
-          {
-              "bmi": 36.37,
-              "createdAt": "2023-10-01"
-          },
-          {
-              "bmi": 37.36,
-              "createdAt": "2023-09-30"
-          }
-      ],
-      "numberFatRecords": [
-          {
-              "bodyFat": 23,
-              "createdAt": "2023-10-17"
-          },
-          {
-              "bodyFat": 57.75,
-              "createdAt": "2023-10-07"
-          },
-          {
-              "bodyFat": 57.75,
-              "createdAt": "2023-10-06"
-          },
-          {
-              "bodyFat": 54.79,
-              "createdAt": "2023-10-05"
-          },
-          {
-              "bodyFat": 51.32,
-              "createdAt": "2023-10-04"
-          },
-          {
-              "bodyFat": 48.1,
-              "createdAt": "2023-10-03"
-          },
-          {
-              "bodyFat": 43.75,
-              "createdAt": "2023-10-02"
-          },
-          {
-              "bodyFat": 41.87,
-              "createdAt": "2023-10-01"
-          },
-          {
-              "bodyFat": 40.1,
-              "createdAt": "2023-09-30"
-          }
-      ],
-      "numberHeartbeatRecords": [
-          {
-              "maxHeartbeat": 145,
-              "createdAt": "2023-10-17"
-          },
-          {
-              "maxHeartbeat": 182,
-              "createdAt": "2023-10-07"
-          },
-          {
-              "maxHeartbeat": 182,
-              "createdAt": "2023-10-06"
-          },
-          {
-              "maxHeartbeat": 183,
-              "createdAt": "2023-10-05"
-          },
-          {
-              "maxHeartbeat": 186,
-              "createdAt": "2023-10-04"
-          },
-          {
-              "maxHeartbeat": 181,
-              "createdAt": "2023-10-03"
-          },
-          {
-              "maxHeartbeat": 149,
-              "createdAt": "2023-10-02"
-          },
-          {
-              "maxHeartbeat": 187,
-              "createdAt": "2023-10-01"
-          },
-          {
-              "maxHeartbeat": 169,
-              "createdAt": "2023-09-30"
-          }
-      ],
-      "numberWorkoutTimeRecords": [
-          {
-              "workoutTime": 1,
-              "workoutTimeKr": "1시간 0분",
-              "createdAt": "2023-10-17"
-          },
-          {
-              "workoutTime": 1,
-              "workoutTimeKr": "1시간 0분",
-              "createdAt": "2023-10-07"
-          },
-          {
-              "workoutTime": 2,
-              "workoutTimeKr": "2시간 0분",
-              "createdAt": "2023-10-06"
-          },
-          {
-              "workoutTime": 1,
-              "workoutTimeKr": "1시간 0분",
-              "createdAt": "2023-10-05"
-          },
-          {
-              "workoutTime": 1.5,
-              "workoutTimeKr": "1시간 30분",
-              "createdAt": "2023-10-04"
-          },
-          {
-              "workoutTime": 1,
-              "workoutTimeKr": "1시간 0분",
-              "createdAt": "2023-10-03"
-          },
-          {
-              "workoutTime": 0.5,
-              "workoutTimeKr": "0시간 30분",
-              "createdAt": "2023-10-02"
-          },
-          {
-              "workoutTime": 1,
-              "workoutTimeKr": "1시간 0분",
-              "createdAt": "2023-10-01"
-          },
-          {
-              "workoutTime": 0.5,
-              "workoutTimeKr": "0시간 30분",
-              "createdAt": "2023-09-30"
-          }
-      ],
-      "numberCompareTime": {
-          "avgTimeTotal": 1.055556,
-          "avgTimeTotalKr": "1시간 3분",
-          "avgTimeUser": 1.055556,
-          "avgTimeUserKr": "1시간 3분"
-      }
-  })
-  });
-});
+//       await AppDataSource.query(`
+//       INSERT INTO users (kakao_id, img_url, user_type)
+//       VALUES ('${mockedId}', '${mockedImgUrl}', 1);
+//       `);
+
+//     //필요한 운동기록 데이터를 생성합니다.  
+//     await AppDataSource.query(`
+//         UPDATE users
+//         SET
+//             nickname = '아무개',
+//             birthday = '1994-04-12',
+//             gender = 'Male',
+//             phone_number = 1072231234,
+//             height = 180.00,
+//             weight = 75.00,
+//             img_url = 'http://k.kakaocdn.net/dn/mIPLJ/btsrYVlrGgO/0nvRL8xGC8ROQd8t5MPG6k/img_640x640.jpg'
+//         WHERE id = 1;
+//     `)
+//        //유저 정보를 생성합니다.  
+//     await AppDataSource.query(`
+//         INSERT INTO workout_records (
+//             user_id, 
+//             water_content, 
+//             workout_time, 
+//             current_weight, 
+//             muscle_mass, 
+//             body_fat, 
+//             max_heartrate, 
+//             created_at) VALUES 
+//         (1, 26, 0.5, 79.1, 33, 40.10, 169, '2023-10-01 00:00:00'),
+//         (1, 24, 1, 74.2, 34, 41.87, 187, '2023-10-02 00:00:00'),
+//         (1, 23, 0.5, 76, 35, 43.75, 149, '2023-10-03 00:00:00'),
+//         (1, 22, 1, 79, 38, 48.10, 181, '2023-10-04 00:00:00'),
+//         (1, 21, 1.5, 76, 39, 51.32, 186, '2023-10-05 00:00:00'),
+//         (1, 27, 1, 73, 40, 54.79, 183, '2023-10-06 00:00:00'),
+//         (1, 30, 2, 71, 41, 57.75, 182, '2023-10-07 00:00:00');
+//     `)
+//   });
+
+//   test("SUCCESS: Able to read the user record", async () => {
+//     await request(app)
+//     .get("/records")
+//     .set("Authorization", mockedAccessToken)
+//     .expect(200);
+//     });
+
+//   afterAll(async () => {
+//     // 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다.
+//     await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 0;`);
+//     await AppDataSource.query(`TRUNCATE TABLE users;`);
+//     await AppDataSource.query(`TRUNCATE TABLE workout_categories;`);
+    
+//     // 모든 테스트가 끝나게 되면(afterAll) DB 커넥션을 끊어줍니다.
+//     await AppDataSource.destroy();
+    
+//   });
+
+
+// });
