014bd48c5e6b64c6abddf930e1a226308704dfed
kimchiro <rlaehddhs12@gmail.com>
2023-10-23 13:31:04 +0900

    fix: 에러해결을 위한 커밋

--- src/services/communityServices.js
+++ src/services/communityServices.js
@@ -1,33 +1,40 @@
+const { throwError } = require("../utils/throwError");
 const { communityDao } = require("../models");
 const { deleteCommentDao } = require("../models/communityDao");
-const { createPostDao, deletePostDao, createCommentDao, isSubscriptDao } =
-  communityDao;
+const {
+  createPostDao,
+  deletePostDao,
+  createCommentDao,
+  getAllPostDao,
+  isSubscriptDao,
+} = communityDao;
 
 const deletePostService = async (userId, postId) => {
   await deletePostDao(userId, postId);
   return "POST_DELETE";
 };
 
-const deleteCommentService = async (userId, content) => {
-  await deleteCommentDao(userId, content);
+const deleteCommentService = async (userId, commentId, postId) => {
+  await deleteCommentDao(userId, commentId, postId);
   return "DELETE_CONTENT";
 };
 
 const getAllPostService = async (userId, postId) => {
-  await getAllPostDao(userId, postId);
-  return "GET_POST";
+  return await getAllPostDao(userId, postId);
 };
 
 const createPostService = async (userId, content, img_url) => {
   const checkSubscript = await isSubscriptDao(userId);
-  if (checkSubscript.length == 0) throwError(400, "NOT_SUBSCRIBER");
+  console.log("userId after isSubscriptDao call:", userId);
+  if (checkSubscript === "false") throwError(400, "NOT_SUBSCRIBER");
   return await createPostDao(userId, content, img_url);
 };
 
-const createCommentService = async (userId, content) => {
+const createCommentService = async (userId, content, postId) => {
+  console.log(userId, content, postId);
   const checkSubscript = await isSubscriptDao(userId);
-  if (checkSubscript.length == 0) throwError(400, "NOT_SUBSCRIBER");
-  await await createCommentDao(userId, content);
+  if (checkSubscript === "false") throwError(400, "NOT_SUBSCRIBER");
+  await createCommentDao(userId, content, postId);
   return "COMMMENT_CREATE";
 };
 
