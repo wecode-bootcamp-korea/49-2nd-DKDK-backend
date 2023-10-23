27d38a20a4c38e61d4874bcd54fd51720e92001d
Seungwon Kim <seungwonkim@Seungwonui-MacBookAir.local>
2023-10-23 12:02:20 +0900

    fix: 에러핸들링을 위해  asyncWrap 미들웨어를 사용합니다.

--- src/routes/recordRouter.js
+++ src/routes/recordRouter.js
@@ -1,4 +1,5 @@
 const recordController = require("../controllers/recordController");
+const { asyncWrap } = require("../middlewares/errorHandler")
 // const auth = require("../middlewares");
 const express = require("express");
 
@@ -7,8 +8,8 @@ const recordRouter = express.Router(); //라우터를 시작합니다.
 const createRecord = recordController.createRecord;
 const readRecord = recordController.readRecord;
 
-recordRouter.post("/", recordController.createRecord);
-recordRouter.get("/:id", recordController.readRecord);
+recordRouter.post("/", asyncWrap(recordController.createRecord));
+recordRouter.get("/:id", asyncWrap(recordController.readRecord));
 
 // recordRouter.post("/", auth, recordController.createRecord);
 // recordRouter.get("/:id", validateToken, recordController.readRecord);
