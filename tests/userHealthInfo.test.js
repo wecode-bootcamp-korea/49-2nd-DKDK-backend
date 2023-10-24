240fc05a800e98142d6994fdd0a40064c98ce957
Rooney Lee <kroonstazy@gmail.com>
2023-10-24 17:26:04 +0900

    test: POST / PATCH 테스트 케이스 작성중

--- tests/userHealthInfo.test.js
+++ tests/userHealthInfo.test.js
@@ -14,7 +14,117 @@ describe("GET User Information", () => {
     await AppDataSource.initialize();
 
     // GET 테스트를 위한 DB필수정보 생성
+    // 1. workout category
+    await AppDataSource.query(
+      `INSERT INTO workout_categories (id, category)
+      VALUES
+      (1, '헬스'),
+      (2, '필라테스'),
+      (3, '요가');
+      `
+    );
+    // 2. USER정보 생성
+    await AppDataSource.query(
+      `INSERT INTO users
+      (id, nickname, birthday, gender, phone_number, user_type, height, weight, interested_workout, workout_load)
+        VALUES
+      (17, '테스터', '2023-10-10', 1, 01012341234, 1, 186.00, 78.00, 1, 1);
+      `
+    );
+    // 3 - 1 . 식단데이터 생성을 위한 음식 타입 생성
+    await AppDataSource.query(
+      `
+      INSERT INTO food_types (id, type) VALUES
+      (1, '밥'),
+      (2, '국'),
+      (3, '메인1'),
+      (4, '메인2'),
+      (5, '반찬');
+      `
+    );
+    // 3 - 2 . 식단데이터 생성을 위한 meal_plan 생성
+    await AppDataSource.query(
+      `
+      INSERT INTO meal_plans (id, grade) VALUES
+      (1, 1400),
+      (2, 1600),
+      (3, 1800)
+      `
+    );
+    // 3 - 3 . 식단데이터 생생
+    await AppDataSource.query(
+      `
+      INSERT INTO foods (id, meal_plan_id, type_id, name, kcal, weight) VALUES
+      (1, 1, 1, '보리밥', 235, 140),
+      (2, 1, 2, '미역쇠고기국', 35, 12),
+      (3, 1, 3, '메추리알장조림', 80, 40),
+      (4, 1, 4, '오이초무침', 30, 70),
+      (5, 1, 5, '마늘종 볶음', 40, 25),
+      (6, 2, 1, '보리밥', 235, 140),
+      (7, 2, 2, '미역쇠고기국', 35, 12),
+      (8, 2, 3, '메추리알장조림', 80, 40),
+      (9, 2, 4, '오이초무침', 30, 70),
+      (10, 2, 5, '마늘종 볶음', 40, 25),
+      (11, 3, 1, '보리밥', 235, 140),
+      (12, 3, 2, '미역쇠고기국', 35, 12),
+      (13, 3, 3, '메추리알장조림', 80, 40),
+      (14, 3, 4, '오이초무침', 30, 70),
+      (15, 3, 5, '마늘종 볶음', 40, 25);
+      `
+    );
+    // 4 - 1 . 운동루틴 생성
+    await AppDataSource.query(
+      `
+      INSERT INTO workouts (id, category_id, name, repetition, \`set\`) VALUES
+      (1, 1, 'Shoulder Press', '20회', 5);
+      `
+    );
+  });
 
+  afterAll(async () => {
+    // 테스트 데이터베이스의 불필요한 데이터를 전부 지워줍니다.
+    // 테이블에 있는 데이터를 날려주는 코드
+    await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 0;`);
+    await AppDataSource.query(`TRUNCATE TABLE users;`);
+    await AppDataSource.query(`TRUNCATE TABLE workout_categories;`);
+    await AppDataSource.query(`TRUNCATE TABLE food_types;`);
+    await AppDataSource.query(`TRUNCATE TABLE meal_plans;`);
+    await AppDataSource.query(`TRUNCATE TABLE foods;`);
+    await AppDataSource.query(`TRUNCATE TABLE workouts;`);
+    await AppDataSource.query(`SET FOREIGN_KEY_CHECKS = 1;`);
+
+    // 모든 테스트가 끝나게 되면(afterAll) DB 커넥션을 끊어줍니다.
+    await AppDataSource.destroy();
+  });
+
+  test("FAILED: GET - NO USER ID", async () => {
+    // supertest의 request를 활용하여 app에 테스트용 request를 보냅니다.
+    const validAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcsImlhdCI6MTY5ODEyMDQ3N30.dD659ouwe5jwD7jINYURXHoWpj7ZWMJRpaAzXTQx75Q"
+    const res = await request(app)
+      .get("/userHealthInfo")
+      .set("Authorization", validAccessToken) // HTTP Method, 엔드포인트 주소를 작성합니다.
+      .expect(400); // expect()로 예상되는 statusCode, response를 넣어 테스트할 수 있습니다.
+
+    expect(res.body.message).toEqual("KEY_ERROR - ID");
+  });
+
+  test("FAILED: GET - NO SUCH USER", async () => {
+    const res = await request(app)
+      .get("/userHealthInfo?userId=999999")
+      .expect(400);
+
+    expect(res.body.message).toEqual("KEY_ERROR_NO_SUCH_USER");
+  });
+});
+
+describe("PATCH / POST - USER_INFO_UPDATE", () => {
+  let app;
+
+  beforeAll(async () => {
+    app = createApp();
+    await AppDataSource.initialize();
+
+    // GET 테스트를 위한 DB필수정보 생성
     // 1. workout category
     await AppDataSource.query(
       `INSERT INTO workout_categories (id, category)
@@ -44,8 +154,7 @@ describe("GET User Information", () => {
       (4, '메인2'),
       (5, '반찬');
       `
-    )
-
+    );
     // 3 - 2 . 식단데이터 생성을 위한 meal_plan 생성
     await AppDataSource.query(
       `
@@ -54,7 +163,7 @@ describe("GET User Information", () => {
       (2, 1600),
       (3, 1800)
       `
-    )
+    );
     // 3 - 3 . 식단데이터 생생
     await AppDataSource.query(
       `
@@ -75,14 +184,20 @@ describe("GET User Information", () => {
       (14, 3, 4, '오이초무침', 30, 70),
       (15, 3, 5, '마늘종 볶음', 40, 25);
       `
-    )
+    );
     // 4 - 1 . 운동루틴 생성
     await AppDataSource.query(
       `
       INSERT INTO workouts (id, category_id, name, repetition, \`set\`) VALUES
       (1, 1, 'Shoulder Press', '20회', 5);
       `
-    )
+    );
+
+    // 5 - 1 일반유저 데이터 업데이트 하기
+    
+    // 5 - 2 트레이너 데이터 업데이트 하기
+
+    // 5 - 3 유저 프로파일 이미지 업데이트
 
   });
 
@@ -102,7 +217,7 @@ describe("GET User Information", () => {
     await AppDataSource.destroy();
   });
 
-  test("FAILED: GET - NO USER ID", async () => {
+  test("SUCCESS: USER INFO UPDATED", async () => {
     // supertest의 request를 활용하여 app에 테스트용 request를 보냅니다.
     const res = await request(app)
       .get("/userHealthInfo") // HTTP Method, 엔드포인트 주소를 작성합니다.
@@ -111,7 +226,7 @@ describe("GET User Information", () => {
     expect(res.body.message).toEqual("KEY_ERROR - ID");
   });
 
-  test("FAILED: GET - NO SUCH USER", async () => {
+  test("SUCCESS: TRAINER INFO UPDATED", async () => {
     const res = await request(app)
       .get("/userHealthInfo?userId=999999")
       .expect(400);
@@ -121,7 +236,7 @@ describe("GET User Information", () => {
 
   // 다음과 같이 본인이 작성한 코드에 맞춰 다양한 케이스를 모두 테스트해야 합니다.
   // 그래야 의도에 맞게 코드가 잘 작성되었는지 테스트 단계에서부터 확인할 수 있습니다!
-  test("SUCCESS: GET - INFO_LOADED", async () => {
+  test("SUCCESS: PROFILE IMG UPLOADED", async () => {
     const res = await request(app).get(`/userHealthInfo?userId=1&workoutRcmdId=1`);
     expect(200);
     expect(res.body.message).toEqual("MYPAGE_LOADED");
@@ -189,4 +304,19 @@ describe("GET User Information", () => {
               }]
     });
   });
+
+  test("SUCCESS: POST - USER_INFO_UPDATED", async () => {
+  })
+
+  test("SUCCESS: POST - USER_INFO_UPDATED", async () => {
+  })
+
+  test("SUCCESS: POST - USER_INFO_UPDATED", async () => {
+  })
+
+  test("SUCCESS: POST - USER_INFO_UPDATED", async () => {
+  })
+
+  test("SUCCESS: POST - USER_INFO_UPDATED", async () => {
+  })
 });
